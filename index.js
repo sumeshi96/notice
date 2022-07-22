29a90741e235150af41907b645bca3125a47de0e
sumeshi96 <yumapc96@gmail.com>
2022-07-22 15:02:29 +0900

    トレトレーズン団子発動時の処理を追加

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ function sound(type, sec) {
     osc.stop(sec)
 }
 
-function OnButtonClick() {
+function OnButton1Click() {
     sound("square", 0.1);
-    setInterval('sound("square", 0.1)', '240000');
-    target = document.getElementById("output");
+    setInterval('sound("square", 0.1)', '240000');//240000ms=4min
+    target = document.getElementById("output1");
+    target.innerHTML = "Starting!";
+}
+
+function OnButton2Click() {
+    sound("square", 0.1);
+    setInterval('sound("square", 0.1)', '180000');//180000ms=3min
+    target = document.getElementById("output2");
     target.innerHTML = "Starting!";
 }
